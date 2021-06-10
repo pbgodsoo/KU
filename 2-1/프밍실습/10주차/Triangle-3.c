c780b4fabf08bc2f35890fdcb9a0baa52a7793d2
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-10 16:11:21 +0900

    Create Triangle-3.c

---
+++ 2-1/프밍실습/10주차/Triangle-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("삼각형의 높이: ");
+    scanf("%d", &n);
+    for(int i = n; i>=1; i--)
+    {
+        for(int j = i ; j>0; j--)
+            printf("*");
+        printf("\n");
+    }
+}
