44ac46b51a9f172561a03c327d3a51fa266d89fa
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-11 14:59:01 +0900

    Create Ex11-8.c

---
+++ 2-1/프밍실습/11주차/Ex11-8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5][5] = {0};
+    int k=1;
+
+    for(int i=0; i<5; i++)
+        for(int j=i; j<5; j++)
+            arr[i][j] = k++;
+    
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+            printf("%2d ", arr[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
