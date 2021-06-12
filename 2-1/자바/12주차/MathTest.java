e9e48b81748e0370738cb98bdfc27ac5041cff94
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-12 11:10:12 +0900

    Create MathTest.java

---
+++ 2-1/자바/12주차/MathTest.java
@@ -0,0 +1,11 @@
+public class MathTest {
+
+	public static void main(String[] args) {
+		System.out.printf("sqrt(900.0)=%.2f\n", Math.sqrt(900.0));
+		System.out.printf("cos(900.0)=%.2f\n", Math.cos(900.0));
+		System.out.printf("log(900.0)=%.2f\n", Math.log(900.0));
+		System.out.printf("floor(9.25)=%.2f\n", Math.floor(9.25));
+		System.out.println("PI=" + Math.PI);
+	}
+
+}
