3da768f5621377759a1769e89c88ed6a6f091759
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-22 22:21:46 +0900

    Create 2주차.java

---
+++ 2-1/자바/과제/2주차.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Addition2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner( System.in );
+		
+		int number1;
+		int number2;
+		int sum;
+
+		System.out.print("Enter first integer: ");
+		number1 = input.nextInt();
+
+		System.out.print("Enter first integer: ");
+		number2 = input.nextInt();
+
+		sum = number1 + number2;
+		System.out.printf("Sum is %o in octal, %d in decimal, %x in hexadecimal", sum, sum, sum);
+	}
+}
