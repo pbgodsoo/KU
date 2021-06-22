396e898b9fb80bc2625c0fc16c8d0a8cf0f22a8d
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-22 22:24:24 +0900

    Create 5주차.java

---
+++ 2-1/자바/과제/5주차.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex05_3{
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter x1, y1 and r1: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double r1 = input.nextDouble();
+		System.out.print("Enter x2, y2 and r2: ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		double r2 = input.nextDouble();
+		double d = Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
+    
+		if (d < (r1+r2))
+			System.out.printf("overlapped\n");
+		else
+			System.out.printf("not overlapped\n");
+
+	}
+}
